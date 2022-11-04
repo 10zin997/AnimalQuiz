d7d8a051b1d0a435a3777167dff47776a824a263
Tenzin wangyal <tenzinwangyal20@yahoo.com>
2022-11-04 13:10:59 -0700

    Initial Commit

---
+++ AnimalQuiz/AnimalQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalQuizApp.swift
+//  AnimalQuiz
+//
+//  Created by Tenzin wangyal on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimalQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
